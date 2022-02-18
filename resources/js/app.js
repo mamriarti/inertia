f8d95e236f4c24f5a3eb12a0f27a0a826a42f85c
Alexandra <mamriarti@gmail.com>
2022-02-18 13:05:02 +0300

    add Progress Indicators

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 
 import { createApp, h } from 'vue'
-import { createInertiaApp } from '@inertiajs/inertia-vue3'
+import { createInertiaApp } from '@inertiajs/inertia-vue3';
+import { InertiaProgress } from '@inertiajs/progress'
 
 createInertiaApp({
     resolve: name => require(`./Pages/${name}`),
@@ -9,4 +10,10 @@ createInertiaApp({
             .use(plugin)
             .mount(el)
     },
-})
+});
+
+InertiaProgress.init(
+    {
+        color: 'red',
+        showSpinner: true,
+    });
--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,11 @@ return Inertia::render('Home', [
 ]);
 });
 Route::get('/users',function (){
+    sleep(2);
        return Inertia::render('Users');
     });
 Route::get('/settings',function (){
+    sleep(2);
         return Inertia::render('Settings');
     });
 
